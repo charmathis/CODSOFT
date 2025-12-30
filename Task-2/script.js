69cc9b398cd97c65bc0bd4a0256ba9f498d6f818
charmathis <charmathis0329@gmail.com>
2025-12-30 20:55:10 +0530

    Add files via upload

---
+++ Task-2/script.js
@@ -0,0 +1,6 @@
+document.getElementById('hire-btn').addEventListener('click', function(){
+    alert('Thank you for your interest! You can contact me at: charu02@gmail.com');
+});
+document.getElementById('resume-btn').addEventListener('click', function () {
+    alert("✅ Resume downloaded successfully!");
+});
